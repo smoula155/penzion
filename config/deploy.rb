c49171ac6d4cb3f4b546ee2bb90ae14e05e431e6
Martin Smolka <martin.smolka@sitd.cz>
2016-03-18 17:18:37 +0100

    test

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :repo_url, 'git@example.com:smoula155/penzion.git'
 # set :pty, true
 
 # Default value for :linked_files is []
- set :linked_files, %w{config/database.yml config/secrets.yml}
+ set :linked_files, %w{config/database.yml config}
 
 # Default value for linked_dirs is []
  set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
@@ -34,25 +34,3 @@ set :repo_url, 'git@example.com:smoula155/penzion.git'
 # Default value for keep_releases is 5
 # set :keep_releases, 5
 
-namespace :deploy do
-
-  desc 'Restart application'
-  task :restart do
-    on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
-    end
-  end
-
-  after :publishing, :restart
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
