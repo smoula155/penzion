186cf8c6d61afc4542f4f98d8d61bffcd817d033
Martin Smolka <smolkamartin155@gmail.com>
2016-03-19 14:01:10 +0100

    uprava obrazku

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,10 +1,6 @@
 class Image < ActiveRecord::Base
 
-    has_attached_file :picture,
-    :storage => :dropbox,
-    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-    :dropbox_options => {},
-    :path => ":style/:id_:filename"
+    has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
 
  	validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
 
