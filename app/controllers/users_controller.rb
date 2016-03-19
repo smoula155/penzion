d1c7ebf34846a32d191361654d7352209f1099ba
Martin Smolka <smolkamartin155@gmail.com>
2016-03-19 14:17:43 +0100

    docasna uprava

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   before_filter :find_user, only: [:edit,:update]
   
-  load_and_authorize_resource
+  #load_and_authorize_resource
   
   def index 
     @users = User.all
