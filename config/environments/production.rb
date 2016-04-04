e1b20e83acf467bb0330871232bdc77d8826fff2
Martin Smolka <smolkamartin155@gmail.com>
2016-04-04 22:51:04 +0200

    uprava mailu

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,8 +80,7 @@ Rails.application.configure do
   #config.action_mailer.default_url_options = { host: "penzionwhitezralok.cz", port: 25}
   config.action_mailer.raise_delivery_errors = true
 
-  # set delivery method to :smtp, :sendmail or :test
-  config.action_mailer.delivery_method = :smtp
+
 
   # these options are only needed if you choose smtp delivery
   config.action_mailer.delivery_method = :smtp
